98708740f65691649eb823c9a236d5e6f0226ab0
1000ch <shogo.sensui@gmail.com>
2015-03-30 11:39:52 +0900

    without cloning

--- src/position-sticky.js
+++ src/position-sticky.js
@@ -4,16 +4,20 @@ import Rectangle from './rectangle';
 import util from './util';
 
 export default class PositionSticky {
-  
+
   constructor(element) {
-    
+
     this.sticky = element;
     this.parent = element.parentNode;
-    this.clone = null;
     this.rectangle = new WeakMap();
 
     this.rectangle.set(this.sticky, new Rectangle(this.sticky));
     this.rectangle.set(this.parent, new Rectangle(this.parent));
+
+    this.diff = {
+      top: this.parent.offsetTop - this.parent.offsetTop,
+      left: this.sticky.offsetLeft - this.parent.offsetLeft
+    };
 
     this.top    = this.sticky.style.top.replace('px', '') - 0;
     this.bottom = this.sticky.style.bottom.replace('px', '') - 0;
@@ -21,55 +25,35 @@ export default class PositionSticky {
     window.addEventListener('scroll', this.onScroll.bind(this));
     window.addEventListener('resize', this.onResize.bind(this));
   }
-  
+
   onScroll(e) {
-    
+
     let sticky = this.rectangle.get(this.sticky);
     let parent = this.rectangle.get(this.parent);
 
     if (window.scrollY + sticky.height + this.bottom > parent.bottom) {
 
-      if (!this.clone) {
-        return;
-      }
-
       util.setStyle(this.parent, {
         position: 'relative'
       });
-      
+
       util.setStyle(this.sticky, {
         position: 'absolute',
         top: '',
-        bottom: '0px',
-        visibility: 'visible'
+        left: '',
+        bottom: '0px'
       });
-
-      document.body.removeChild(this.clone);
-      this.clone = null;
 
     } else if (window.scrollY >= sticky.top - this.top) {
 
-      if (!this.clone) {
-        this.clone = this.sticky.cloneNode(true);
-        document.body.appendChild(this.clone);
-      }
-
-      util.setStyle(this.clone, {
+      util.setStyle(this.sticky, {
         position: 'fixed',
-        top: this.top + 'px',
-        left: sticky.left + 'px',
+        top: this.top + this.diff.top + 'px',
+        left: parent.left + this.diff.left + 'px',
         width: sticky.width + 'px'
       });
 
-      util.setStyle(this.sticky, {
-        visibility: 'hidden'
-      });
-
     } else {
-      
-      if (!this.clone) {
-        return;
-      }
 
       util.setStyle(this.parent, {
         position: ''
@@ -77,16 +61,12 @@ export default class PositionSticky {
 
       util.setStyle(this.sticky, {
         position: '',
-        bottom: '',
-        visibility: 'visible'
+        bottom: ''
       });
-
-      document.body.removeChild(this.clone);
-      this.clone = null;
     }
   }
-  
-  onResize(e) { 
+
+  onResize(e) {
 
     this.rectangle.set(this.sticky, new Rectangle(this.sticky));
     this.rectangle.set(this.parent, new Rectangle(this.parent));
